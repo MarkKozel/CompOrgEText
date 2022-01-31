ea4f7eac4539c5351b5718e7e7c2538b13fba040
M Kozel Dev <MarkKozel65@gmail.com>
2022-01-30 18:20:33 -0800

    Set Last Used plugin to PST

--- docs/etext/.vuepress/config.js
+++ docs/etext/.vuepress/config.js
@@ -164,7 +164,7 @@ module.exports = {
     [
       '@vuepress/last-updated', {
         dateOptions: {
-          timeZone: 'UTC',
+          timeZone: 'PST',
           timeZoneName: 'short'
         }
       }
