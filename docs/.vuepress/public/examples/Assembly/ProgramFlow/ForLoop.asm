7c6f775af82b0502e9ec797dfdb4a54389c03020
Mark Kozel <markkozel+dev@fastmail.com>
2022-11-03 20:11:50 -0700

    Updates after class review

--- docs/.vuepress/public/examples/Assembly/ProgramFlow/ForLoop.asm
+++ docs/.vuepress/public/examples/Assembly/ProgramFlow/ForLoop.asm
@@ -1,10 +1,13 @@
+;Updated 11/3/2022
 .ORIG x3000
-  ADD R0, R0, #0  ;Load i with 0
-  AND R1, R1, #0  ;Clear temp register
+  AND R0, R0, #0  ;Load x with 0
+  AND R1, R1, #0  ;Load i with 0
 
   ; for(i = 0; i < 10; i++)
-  ForLoop ADD R0, R0, #1    ;Add 1 to i
-          ADD R1, R0, #-10  ;Subtract 10 from i. R1 is a temp register
-          BRn ForLoop       ;Loop again if i is still positive
-  HALT
+  ForLoop 
+          ADD R0, R0, #1    ;Do work...x += 1
+          ADD R1, R0, #-10  ;Test to loop again...Update i with x - 10
+          BRn ForLoop       ;Loop again if i is negative
+                            ;Fall through to Done if i is 0 or positive
+  Done HALT
 .END
